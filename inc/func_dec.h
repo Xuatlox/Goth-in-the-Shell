ffb4d1f9f18b5c2bb8ba93c345ea85f022d37c9a
ansimonn <ansimonn@student.42angouleme.fr>
2026-04-08 18:38:00 +0200

    exec/ exec_cd first version

--- inc/func_dec.h
+++ inc/func_dec.h
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 15:15:32 by ansimonn          #+#    #+#             */
-/*   Updated: 2026/04/08 15:31:00 by ansimonn         ###   ########.fr       */
+/*   Updated: 2026/04/08 17:40:25 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include "struct.h"
 
-void	exec_cd(const t_command *cmd, int fd_in, int fd_out);
+int		exec_cd(const t_command *command, int fd_out, t_env *env);
+char	**get_env(t_env *env, const char *name);
 
 #endif
---
+++ src/cd.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 15:13:51 by ansimonn          #+#    #+#             */
+/*   Updated: 2026/04/08 18:37:02 by ansimonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../inc/minishell.h"
+
+static void	set_env(const char *var_name, char *new_val, t_env *env)
+{
+	char	**var_val;
+
+	var_val = get_env(env, var_name);
+	if (var_val)
+		*var_val = new_val;
+}
+
+static void	cd_old_wd(t_env *env, char **dest, int fd_out)
+{
+	char	**path;
+	int		size;
+
+	path = get_env(env, "OLDPWD");
+	if (path)
+		*dest = *path;
+	size = ft_strlen(*dest);
+	write(fd_out, *dest, size);
+	write(fd_out, "\n", 1);
+}
+
+static void	cd_home(const t_command *cmd, t_env *env, char **dest)
+{
+	char	**home;
+
+	home = get_env(env, "HOME");
+	if (home)
+		*dest = *home;
+}
+
+int	exec_cd(const t_command *cmd, int fd_out, t_env *env)
+{
+	char	*path;
+	char	*dest;
+
+	if (!cmd->next)
+		cd_home(cmd, env, &dest);
+	else if (!ft_strncmp(cmd->next->str, "-", 2))
+		cd_old_wd(env, &dest, fd_out);
+	else
+		dest = cmd->next->str;
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (0);
+	if (chdir(dest))
+	{
+		free(path);
+		return (0);
+	}
+	set_env("OLDPWD", path, env);
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (0);
+	set_env("PWD", path, env);
+}
