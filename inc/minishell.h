4946f8a07bddf5d8f3eb7cf868912a9c38b58644
ansimonn <ansimonn@student.42angouleme.fr>
2026-04-08 11:00:54 +0200

    minishell.h added

---
+++ inc/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 10:57:56 by ansimonn          #+#    #+#             */
+/*   Updated: 2026/04/08 10:59:27 by ansimonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "struct.h"
+
+#endif
