d4c69f7c7993264ea2ba26ccb1b032b9930a41d9
ManoPotame <manolocrenn29@gmail.com>
2026-04-13 05:08:58 +0200

    feat: Adding a basic main to try the minishell funtion

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
+/*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 11:07:18 by ansimonn          #+#    #+#             */
-/*   Updated: 2026/04/09 15:55:25 by ansimonn         ###   ########.fr       */
+/*   Updated: 2026/04/13 03:28:23 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 int	main(void)
 {
-	return (0);
+	char		*line;
+	int			i;
+	t_status	status;
+	t_token		*lex;
+
+	status = SUCCESS;
+	i = 0;
+	while (i != 3)
+	{
+		line = readline("Write input: ");
+		lex = lexer(line, &status);
+		while (lex)
+		{
+			printf("New Node:\n");
+			while (lex->cmd)
+			{
+				printf("CMD: %s\n", lex->cmd->str);
+				lex->cmd = lex->cmd->next;
+			}
+			lex = lex->next;
+		}
+		i++;
+	}
 }
