b0639c2b75d10115af9fb913f1627b4a9736fc35
ManoPotame <manolocrenn29@gmail.com>
2026-05-04 14:17:36 +0200

    Faute d'antoine.

--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,21 @@
 /*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 11:07:18 by ansimonn          #+#    #+#             */
-/*   Updated: 2026/04/24 17:52:53 by mcrenn           ###   ########.fr       */
+/*   Updated: 2026/05/04 13:47:30 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv/* , char **envp */)
 {
 	char		*line;
 	int			i;
 	t_status	status;
 	t_token		*tkn_node;
 
+	(void)argc;
+	(void)argv;
 	status = SUCCESS;
 	i = 0;
 	while (i != 5)
@@ -26,8 +28,8 @@ int	main(void)
 		line = readline("Write input: ");
 		if (line[0] != 0)
 			add_history(line);
-		tkn_node = parsing(line, &status);
-		lst_clear_tkn(&tkn_node);
+		tkn_node = parsing(line, &status/* , envp */);
+		lst_clear_tkn(&tkn_node);// Ephemere
 		i++;
 	}
 	rl_clear_history();
