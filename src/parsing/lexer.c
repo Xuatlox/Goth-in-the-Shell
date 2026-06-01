e896a659cb3f9d570679b5dfd2dfbb06f57beb72
ManoPotame <manolocrenn29@gmail.com>
2026-06-01 09:41:38 +0200

    norm: lexer.c now follow the norm.

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -6,37 +6,37 @@
 /*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 03:28:47 by mcrenn            #+#    #+#             */
-/*   Updated: 2026/05/28 16:43:42 by ansimonn         ###   ########.fr       */
+/*   Updated: 2026/06/01 09:38:15 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-t_token	*lexer(char* cmd, t_status *status, t_minishell *shell)
+t_token	*lexer(char *cmd, t_status *status, t_minishell *shell)
 {
 	size_t			i;
-	t_token			*tkn_node;
+	t_token			*tkn_n;
 	t_quote_state	quote_state;
 
 	i = 0;
 	quote_state = NO_QTE;
-	tkn_node = lst_newtoken(status);
+	tkn_n = lst_newtoken(status);
 	while (cmd && cmd[i] && *status == SUCCESS)
 	{
 		check_quotes(cmd[i], &quote_state);
 		if (cmd[i] == '|' && quote_state == NO_QTE && *status == SUCCESS)
-			ft_lstadd_token(&tkn_node, status);
+			ft_lstadd_token(&tkn_n, status);
 		else if (ft_isspace(cmd[i]) == 1
 			&& quote_state == NO_QTE && *status == SUCCESS)
-			ft_lstadd_command(ft_lstlast_token(tkn_node), 0, status);
+			ft_lstadd_command(ft_lstlast_token(tkn_n), 0, status);
 		else if ((cmd[i] == '<' || cmd[i] == '>') && quote_state == NO_QTE
 			&& *status == SUCCESS)
-			*status = redirect_manager(cmd, ft_lstlast_token(tkn_node), &i, shell);
+			*status = redirect_manager(cmd, ft_lstlast_token(tkn_n), &i, shell);
 		else
-			ft_lstadd_command(ft_lstlast_token(tkn_node), cmd[i], status);
+			ft_lstadd_command(ft_lstlast_token(tkn_n), cmd[i], status);
 		if (cmd && cmd[i] && *status == SUCCESS)
 			i++;
 	}
 	free(cmd);
-	return (tkn_node);
+	return (tkn_n);
 }
