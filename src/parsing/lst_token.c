67b26c7e562c84f92010d497c84b7e8cb23b59f3
ManoPotame <manolocrenn29@gmail.com>
2026-06-01 10:30:25 +0200

    norm: lst_token.c now follow the norm.

--- src/parsing/lst_token.c
+++ src/parsing/lst_token.c
@@ -6,7 +6,7 @@
 /*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 14:57:02 by mcrenn            #+#    #+#             */
-/*   Updated: 2026/04/22 17:25:37 by mcrenn           ###   ########.fr       */
+/*   Updated: 2026/06/01 10:29:43 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_lstadd_token(t_token **lst, t_status *status)
 		ft_lstlast_token(*lst)->next = lst_newtoken(status);
 }
 
-t_token		*lst_newtoken(t_status *status)
+t_token	*lst_newtoken(t_status *status)
 {
 	t_token	*token;
 
